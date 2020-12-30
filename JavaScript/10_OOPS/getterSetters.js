611349c863575c3e4ae7fbb867f7dcae4c792d4b
Vishal Ramane <vramane007@gmail.com>
2020-12-30 16:39:28 +0530

    Getter-setter methods

---
+++ JavaScript/10_OOPS/getterSetters.js
@@ -0,0 +1,20 @@
+'use strict';
+// Setters and Getters
+
+const account = {
+  owner: 'Jonas',
+  movements: [200, 530, 120, 300],
+
+  get latest() {
+    return this.movements.slice(-1).pop();
+  },
+
+  set latest(mov) {
+    this.movements.push(mov);
+  },
+};
+
+console.log(account.latest);
+
+account.latest = 50;
+console.log(account.movements);
