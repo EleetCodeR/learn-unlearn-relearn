9aee3cafdd10dc720515ab3adce8c5b41f85c367
Vishal Ramane <vramane007@gmail.com>
2021-04-07 17:41:15 +0530

    removed deprecated call.

--- NodeJS_CompleteGuide/ExpressJS/util/path.js
+++ NodeJS_CompleteGuide/ExpressJS/util/path.js
@@ -1,6 +1,7 @@
 const path = require("path");
 
-module.exports = path.dirname(process.mainModule.filename);
+// module.exports = path.dirname(process.mainModule.filename);
+module.exports = path.dirname(require.main.filename);
 
-// using 'process' glob.var find the main-file name.
+// using 'require' glob.var find the main-file name.
 // then from the main file name, we get its directory name.
