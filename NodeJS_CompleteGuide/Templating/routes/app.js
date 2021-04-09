bf4d36e36e243a9ab2f5c5e8edb4a5ce42d39f13
Vishal Ramane <vramane007@gmail.com>
2021-04-09 17:17:00 +0530

    rendering views - templating engines

--- NodeJS_CompleteGuide/Templating/routes/app.js
+++ NodeJS_CompleteGuide/Templating/routes/app.js
@@ -27,7 +27,7 @@ app.use("/admin", adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.sendFile(path.join(rootDir, "..", "views", "404.html"));
+  res.status(404).render("404");
 });
 
 app.listen(3000);
