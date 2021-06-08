15aceddab3cc1e54a8c4f0ea0fa8dfa666d56410
Vishal Ramane <vramane007@gmail.com>
2021-06-08 18:29:19 +0530

    typo fix

--- DS-Algo/BST/validateBST.js
+++ DS-Algo/BST/validateBST.js
@@ -1,27 +1,23 @@
-
 class BST {
-    constructor(value) {
-      this.value = value;
-      this.left = null;
-      this.right = null;
-    }
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
   }
-  
-  // O(n) time | O(d) space d- depth of the tree.
-  function validateBst(tree) {
-    return validateBstHelper(tree.-Infinity,Infinity);
-  }
-  
-  
-  function validateBstHelper(tree,min,max){
-      if(tree === null) return true;
-      if(tree.value < min || tree.value >= max) return false;
-      const leftIsValid =  validateBstHelper(tree.left,min,tree.value);
-      const rightIsValid =  validateBstHelper(tree.right,tree.value,max);
-      return leftIsValid && rightIsValid;
-  }
-  
+}
 
-  exports.BST = BST;
-  exports.validateBst = validateBst;
-  
+// O(n) time | O(d) space d- depth of the tree.
+function validateBst(tree) {
+  return validateBstHelper(tree, -Infinity, Infinity);
+}
+
+function validateBstHelper(tree, min, max) {
+  if (tree === null) return true;
+  if (tree.value < min || tree.value >= max) return false;
+  const leftIsValid = validateBstHelper(tree.left, min, tree.value);
+  const rightIsValid = validateBstHelper(tree.right, tree.value, max);
+  return leftIsValid && rightIsValid;
+}
+
+exports.BST = BST;
+exports.validateBst = validateBst;
